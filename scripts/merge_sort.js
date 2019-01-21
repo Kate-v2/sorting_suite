ddda61f01f94cce168d4b7018202a0b3309f6aee
Katelyn <katelyn.rimkunas@gmail.com>
2019-01-20 17:57:16 -0700

    dump

--- scripts/merge_sort.js
+++ scripts/merge_sort.js
@@ -5,15 +5,69 @@ module.exports = {
 
   mergeSort: function(arr) {
     let length = arr.length
-    return length == 1 ? arr : this.sort(arr, length)
+    return length == 1 ? arr : this.sort(arr)
   },
 
-  sort: function(arr, length) {
-    let mid = this.middle(length)
-    let left = this.breakArr(0, mid)
-    let right = this.breakArr(arr, mid+1, length)
-    return left
+  sort: function(arr, newArr = []) {
+    // return arr if arr.length == 1
+    // return this.addToArr(arr, newArr) if (arr.length == 1)
+    // return newArr.push(arr[0]) if arr.length == 1
+    // arr.length == 1 ? return this.addToArr(arr, newArr) : null
+    if (arr.length == 1) {
+      return this.addToArr(arr, newArr)
+    }
+
+    // let mid = this.middle(length)
+    //
+    // let left = this.breakArr(0, mid)
+    // let lefty = this.sort(left, newArr)
+    //
+    //
+    // let right = this.breakArr(arr, mid+1, length)
+    // let righty = this.sort(right, newArr)
+    //
+    // sort = this.addToArr(lefty, righty)
+    // left.length > 1 ? this.sort(left, newArr) : this.compare(newArr, left)
+    // let lefty = this.sort(left)
+
+
+    // let righty = this.sort(right)
+    // right.length > 1 ? this.sort(right) : return right
   },
+
+
+  addToArr: function(left, right) {
+
+    // let arr = []
+    // let sizeL = left.length
+    // let sizeR = right.length
+    //
+    // let length = sizeL > sizeR ? sizeL : sizeR
+    //
+    //
+    //
+    // // for(l=0, r=0; x<length; x++){
+    //
+    //
+    //
+    //   if (left.length !=0 && left[l] <= right[r]) {
+    //   // if (left[l] <= right[r]) {
+    //     arr[x] = left[l]
+    //     l++
+    //   } else {
+    //     arr[x] = right[r]
+    //     r++
+    //   }
+    //
+    // }
+    // return arr
+  },
+
+
+
+
+
+  // --- Helper methods ----
 
   breakArr: function(arr, start, end) {
     let piece = []
--- test/merge_sort_test.js
+++ test/merge_sort_test.js
@@ -1,4 +1,5 @@
 const assert = require('chai').assert;
+const expect = require('chai').expect;
 
 const sort = require('../scripts/merge_sort')
 
@@ -11,34 +12,58 @@ describe('Merge Sort', function() {
       unsorted = [4, 3, 2, 1]
       expected = [1, 2, 3, 4]
       sorted = sort.mergeSort(unsorted)
-      assert( true, sorted === expected)
+      this.skip('Not Able to Sort Yet')
+      expect(sorted).to.eql(expected)
     });
 
-    // it('can sort', function() {});
+    // it('can sort', function() {
+    //   small = [2,1,100,3]
+    //   // expected = [1,2,3, 100]
+    //   expected = 'blah'
+    //   sorted = sort.sort(small)
+    //   // pry = require('pryjs')
+    //   // eval(pry.it)
+    //   assert(true, sorted === expected)
+    // });
+
+
+    it('can join & sort arrays', function() {
+      left = [1]
+      right = []
+      expected = [1]
+      sorted = sort.addToArr(left, right)
+      expect(sorted).to.eql(expected)
+
+
+    });
+
+
+
+
+    // --- Helper methods ----
 
     it('can break an array', function() {
       unsorted = [4, 3, 2, 1, 0]
       expected = [4, 3]
       left = sort.breakArr(unsorted, 0, 2)
-      assert(true, left == expected )
+      expect(left).to.eql(expected)
       expected = [2, 1, 0]
-      right = sort.breakArr(unsorted, 2+1, 5)
-      assert(true, right == expected )
+      right = sort.breakArr(unsorted, 2, 5)
+      expect(right).to.eql(expected)
     });
 
     it('can find the middle of array', function() {
       even = 4
       expected = 2
       mid = sort.middle(even)
-      assert(true, mid == expected)
+      expect(mid).to.eql(expected)
       odd = 5
       expected = 2
       mid = sort.middle(even)
-      assert(true, mid == expected)
+      expect(mid).to.eql(expected)
     });
 
   });
-
 
 
 
